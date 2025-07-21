6fd18a71b5179db3a3be4cc44423f01b4ea050e9
Muhammad Noor Yusuf <mnyusuf14@gmail.com>
2025-07-22 09:17:44 +0930

    Model

--- routes/web.php
+++ routes/web.php
@@ -1,64 +1,26 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/posts', function () {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-atikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Yusuf',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta, quasi quidem rem,
-            aliquid sed quisquam
-            earum, sequi velit repellendus dolore corporis ut? Est at cupiditate cumque laboriosam tempore iure dolorum.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-atikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Muhammad',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla praesentium et
-            distinctio dolorem quidem itaque ex ipsum blanditiis, labore possimus eligendi voluptates officiis. Facilis
-            quam adipisci tempora suscipit amet modi?'
-        ]
-    ];
+    $posts = Post::all();
 
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-atikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Yusuf',
-            'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta, quasi quidem rem,
-            aliquid sed quisquam
-            earum, sequi velit repellendus dolore corporis ut? Est at cupiditate cumque laboriosam tempore iure dolorum.'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-atikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Muhammad',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla praesentium et
-            distinctio dolorem quidem itaque ex ipsum blanditiis, labore possimus eligendi voluptates officiis. Facilis
-            quam adipisci tempora suscipit amet modi?'
-        ]
-    ];
 
-    $post = Arr::first($posts, function ($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
 
-    if (!$post) abort(404);
+    $post = Post::find($slug);
+
+
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
