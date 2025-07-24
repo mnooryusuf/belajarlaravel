23fc651e45097c5366719c0d2f91882451cc30ff
Muhammad Noor Yusuf <mnyusuf14@gmail.com>
2025-07-24 09:30:49 +0930

    Update UI Menggunakan FlowBite

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -15,16 +15,19 @@ class CategorySeeder extends Seeder
     {
         Category::create([
             'name' => 'Web Design',
-            'slug' => 'web-design'
+            'slug' => 'web-design',
+            'color' => 'bg-red-100'
         ]);
 
         Category::create([
             'name' => 'Web Programming',
-            'slug' => 'web-programming'
+            'slug' => 'web-programming',
+            'color' => 'bg-green-100'
         ]);
         Category::create([
             'name' => 'Artificial Intelligence',
-            'slug' => 'ai'
+            'slug' => 'ai',
+            'color' => 'bg-blue-100'
         ]);
     }
 }
