121fbd15faf1805dc3e756c8baa00a6444a17b9f
Muhammad Noor Yusuf <mnyusuf14@gmail.com>
2025-07-29 16:05:55 +0930

    Simple Post Searching

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,17 +1,24 @@
 <x-layout :title="$title">
 
-    {{-- @foreach ($posts as $post)
-        <article class="py-8 max-w-screen-md border-b border-gray-300">
-            <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
+    <div class="py-4 px-4 mx-auto max-w-screen-xl  lg:px-6">
 
-                <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
-            </a>
-            <div class="text-base text-gray-500">
-                By <a class="hover:underline text-gray-900"
-                    href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in
-                <a class="hover:underline text-gray-900"
-                    href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a> | 1
-                January 2025
+
+        <form class="mb-8 max-w-md mx-auto">
+            <label for="default-search"
+                class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div class="relative">
+                <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                </div>
+                <input type="search" id="default-search"
+                    class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Search post title..." required autofocus />
+                <button type="submit"
+                    class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}
             </p>
